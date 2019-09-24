115319b617e2bfa327f0f268acc2a89ac5381966
mjstthomas0516 <mjstthomas0516@gmail.com>
2019-09-23 17:52:17 -0700

    finished

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed")
+});
+
+$("ul").on("click", ".trash", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+	})
+$("input[type= 'text']").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span class= 'trash'><i class='far fa-trash-alt'></i></span> "+ todoText + "</li>")
+	}
+})
